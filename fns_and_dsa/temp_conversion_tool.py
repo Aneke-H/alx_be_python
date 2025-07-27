4ab288456f8361e6129e42d46d29f218dc11fcc9
Aneke-H <anekendubuisihenry@gmail.com>
2025-07-27 14:28:31 +0100

    added temp_conversion_tool.py

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,32 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    """
+    Converts a temperature from Fahrenheit to Celsius.
+    """
+    # global FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+
+    return f"{fahrenheit}°F is {(fahrenheit - 32)  * FAHRENHEIT_TO_CELSIUS_FACTOR}°C"
+
+def convert_to_fahrenheit(celsius):
+    """
+    Converts a temperature from Celsius to Fahrenheit.
+    """
+    # global CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+    return f"{celsius}°C is {celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32}°F"
+def main():
+    """Entry point of the program"""
+    temperature= float(input("Enter the temperature to convert: "))
+    temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+    if temperature_unit == 'F':
+        return convert_to_celsius(temperature)
+    elif temperature_unit == 'C':
+        return convert_to_fahrenheit(temperature)
+    else:
+        return "Invalid Input, Please try again."
+
+
+if __name__=="__main__":
+    main()
